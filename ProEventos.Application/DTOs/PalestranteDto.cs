8dd055b882091ab439ffc9c49416cda0f28c4c1a
Wanderlei Santos <wanderleisantosdias@gmail.com>
2021-05-03 22:49:25 -0300

    feat: Created Dtos

--- ProEventos.Application/DTOs/PalestranteDto.cs
+++ ProEventos.Application/DTOs/PalestranteDto.cs
@@ -2,18 +2,15 @@
 
 namespace ProEventos.Application.DTOs
 {
-    public class EventoDto
+    public class PalestranteDto
     {
         public int Id { get; set; }
-        public string Local { get; set; }
-        public string DataEvento { get; set; }
-        public string Tema { get; set; }
-        public int QtdPessoas { get; set; }
+        public string Nome { get; set; }
+        public string MiniCurriculo { get; set; }
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
-        public IEnumerable<LoteDto> Lotes { get; set; }
         public IEnumerable<RedeSocialDto> RedesSociais { get; set; }
-        public IEnumerable<PalestranteDto> Palestrantes { get; set; }
+        public IEnumerable<PalestranteDto> Palestrantess { get; set; }
     }
 }
